4dda6b8b309357a022786181114a006846339c20
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-09-10 13:10:49 +0530

    primitives

---
+++ primitive_data_types/index.js
@@ -0,0 +1,45 @@
+// immutable
+
+var str = "Praveen";
+
+//basically string  collection of characters and it store the characters by array format and array stores value by index format 
+//  so in array if we change or reassign the value so we change by index number
+// let see here
+
+var arr =["hi", "bye", "kay"];
+arr[0]= "kalua";
+console.log(arr) // value of arr ["kalua","bye",kay];
+
+// if we want to change value of string its not possible '
+
+// lets see her 
+
+str[2] = "s";
+
+console.log(str) // value of str still Praveen
+
+
+//Number 
+
+// number is help us to calculate arithmetic problems .number is numeric value 
+// in js they provide us built in methods to calculate by using Math object;
+
+// and one important thing in javascript we do not need to specify the data type of declaring a variable
+// because javascript is loosely typed ans dynamic language
+
+// Boolean 
+// boolean is very less memory consumptable data type because it take only i bit
+// its value either 0,or 1, 0 means falsy value , or 1 meas truly 
+
+// Null 
+// Null is very important data type it shows the existence of data inside not primitive data type specially Objects 
+//null means container of data types available but there is not data it means container is empty;
+
+// Undefined 
+
+// Undefined is a special value thats shows us if you are not assigning any value declaring time javascript
+// automatically assign Undefined behind the seen have a good story 
+//
+  
+
+ 
